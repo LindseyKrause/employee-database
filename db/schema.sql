0043c5d8879ad8ae8dc44c9d3309eaa7f04bea88
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-06-05 12:22:07 -0700

    Fixed DB bug/sql commands

--- db/schema.sql
+++ db/schema.sql
@@ -22,5 +22,5 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL, 
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
 );
