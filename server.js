0c381348646a520bab28297c03d5becdd68f5940
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-06-05 20:12:22 -0700

    consolidated code

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // Constants/Dependencies ------------------------
 const mysql = require('mysql2');
-const runInquirer = require('./index');
 require('dotenv').config();
 const PASSWORD = process.env.PASSWORD;
 const cTable = require("console.table");
-
+const inquirer = require('inquirer');
 
 // Connect to Database----------------------------
 const db = mysql.createConnection(
@@ -19,6 +18,39 @@ const db = mysql.createConnection(
 
 db.connect(err => {if (err) throw err});
 
-runInquirer();
 
-module.exports = db;
+//Inquirer Prompt & Questions---------------------------------
+function runInquirer() {inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'chooseAction',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Role', 'Add an Employee', 'Update Employee'],
+            default: 'View All Departments',
+        },
+        {
+            type: 'confirm',
+            name: 'didYou',
+            message: 'Did you do it?',
+            when: (answers) => answers.chooseAction === 'View All Roles'
+        }
+    ])
+    .then(function (data) {
+        console.log('end of inquirer page view departments function is below');
+        viewAllDept(data);
+    })};
+    viewAllDept = function (data) {
+        // console.log(data);
+        if(data.chooseAction === 'View All Departments') {
+            console.log('View All Departments function starting');
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log("hello");
+                });
+            };
+    };
+    
+runInquirer();
